3056da76540261d8c62601b2f451b0f4893cc13b
Nischal <96775797+NischalRapiReddy@users.noreply.github.com>
2022-01-19 09:23:11 +0530

    Add files via upload

---
+++ uninstall.bat
@@ -0,0 +1,27 @@
+@echo off
+echo *****************
+echo Author: @Nischal
+echo *****************
+echo Removing Patch
+if exist "%APPDATA%\Spotify\chrome_elf_bak.dll" (
+    del /s /q "%APPDATA%\Spotify\chrome_elf.dll" > NUL 2>&1
+    move "%APPDATA%\Spotify\chrome_elf_bak.dll" "%APPDATA%\Spotify\chrome_elf.dll" > NUL 2>&1
+) else (
+    echo done
+)
+
+if exist "%APPDATA%\Spotify\Apps\zlink.spa.bak" (
+    del /s /q "%APPDATA%\Spotify\Apps\zlink.spa" > NUL 2>&1
+    move "%APPDATA%\Spotify\Apps\zlink.spa.bak" "%APPDATA%\Spotify\Apps\zlink.spa" > NUL 2>&1
+) else (
+    echo done
+)
+
+if exist "%APPDATA%\Spotify\Apps\xpui.spa.bak" (
+    del /s /q "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
+    move "%APPDATA%\Spotify\Apps\xpui.spa.bak" "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
+) else (
+    echo done
+)
+pause
+© 2022 Nishi-Enterprises, Inc.
